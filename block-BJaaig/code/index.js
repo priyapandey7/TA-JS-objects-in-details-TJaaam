9cbcf1a9ae8dd2c2f7dc4e2f27565994da4faef9
priya pandey <priyarock746@gmail.com>
2021-08-24 15:17:16 +0530

    Assignment

---
+++ block-BJaaig/code/index.js
@@ -0,0 +1,25 @@
+let user = {
+  username: "John",
+  sayHello(message = "Hello") {
+    console.log(message + " " + this.username);
+  },
+};
+
+let user2 = {
+  username: "Arya",
+  sayHello(message = "Hello") {
+    console.log(message + " " + this.username);
+  },
+};
+let user3 = {
+  username: "Bran",
+};
+
+function MainUser() {
+  this.username = "Tyrion";
+  this.sayHello = function sayHello(message = "Hello") {
+    console.log(message + " " + this.username);
+  };
+}
+
+let userSayHello = user.sayHello;
